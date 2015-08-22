9eaf7b53e8f5fcfb90badc83f54e291ff56ac287
Tomas Aparicio <tomas@aparicio.me>
2015-08-22 16:41:27 +0100

    fix(test): gcc

--- semver.c
+++ semver.c
@@ -366,12 +366,11 @@ semver_lte (semver_t x, semver_t y) {
 
 int
 semver_satisfies (semver_t x, semver_t y, const char *operator) {
-  printf("Load operator: %s\n", operator);
   int len = strlen(operator);
   if (len == 0) return 0;
 
   // Infer the comparison operator
-  int op[2];
+  int op[2] = {};
   len = len > 2 ? 2 : len;
   for (int i = 0; i < len; i++) {
     op[i] = (int) operator[i];
@@ -379,8 +378,6 @@ semver_satisfies (semver_t x, semver_t y, const char *operator) {
 
   // Compare based on the specific operator
   if (op[0] == (int) SYMBOL_GT) {
-    //printf("\n>>> Operator: %c == %c => %d\n", op[0], SYMBOL_GT, op[0] == (int) SYMBOL_GT);
-    printf(">>> Operator: %c == %c => %d\n\n", op[1], SYMBOL_EQ, op[1] == (int) SYMBOL_EQ);
     if (op[1] == (int) SYMBOL_EQ) {
       return semver_gte(x, y);
     }
